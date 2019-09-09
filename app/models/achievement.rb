b1c30e9336a34b6218ddc9b32b9eb6fdbf5e0ee0
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-09-09 11:02:54 -0300

    Update 'Achievement' model
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,8 +1,18 @@
 class Achievement
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   belongs_to :user
-  field :name, type: String
+  field :category, type: String
+  field :level, type: Integer
 
   searchkick
+
+  def search_data
+    {
+      user_id: user.id.to_s,
+      created_at: created_at,
+      updated_at: updated_at
+    }
+  end
 end
--- app/models/achievement_category.rb
+++ app/models/achievement_category.rb
@@ -1,8 +1,16 @@
 class AchievementCategory
   include Mongoid::Document
+  include Mongoid::Timestamps
 
-  field :name, type: String
+  field :category, type: String
   field :level, type: Integer
 
   searchkick
+
+  def search_data
+    {
+      created_at: created_at,
+      updated_at: updated_at
+    }
+  end
 end
