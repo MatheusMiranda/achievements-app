79f79e72e72396248c496b14c1d9a22b2219f5dd
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-09-09 22:28:41 -0300

    Add method to update user Achievements
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
 
   field :name, type: String
   validates_uniqueness_of :name
@@ -13,7 +14,28 @@ class User
 
   def search_data
     {
-      name: name
+      name: name,
+      created_at: created_at,
+      updated_at: updated_at
     }
+  end
+
+  def as_json(_opts = {})
+    {
+      id: id,
+      name: name,
+      achievements: get_achievements,
+    }
+  end
+
+  def update_achievements(item, value)
+    achievements = Achievement.search id.to_s, fields: [:user_id], order: {created_at: :desc}, where: {name: item}
+    last_achievement_level = achievements.first.level
+
+    new_achievements = AchievementCategory.search item, fields: [:category], where: {level: {gt: last_achievement_level, lt: value + 1}}
+
+    new_achievements.each do |new_achievement|
+      Achievement.create(user: self, category: new_achievement.category, level: new_achievement.level)
+    end
   end
 end
