f0c4e4270192c7b95190b385385fe1bd487038d8
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-09-09 21:52:46 -0300

    Add achievements to seed
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,21 +3,22 @@ puts "CREATING ACHIEVEMENTS CATEGORIES"
 AchievementCategory.reindex
 coins_levels = [1, 100, 1000, 10000, 100000]
 coins_levels.each do |coin_level|
-  AchievementCategory.create(name: "coins", level: coin_level)
+  AchievementCategory.create(category: "coins", level: coin_level)
 end
 
 deaths_levels = [1, 10, 25, 50, 100]
 deaths_levels.each do |death_level|
-  AchievementCategory.create(name: "deaths", level: death_level)
+  AchievementCategory.create(category: "deaths", level: death_level)
 end
 
 monsters_levels = [1, 100, 1000, 10000, 100000]
 monsters_levels.each do |monster_level|
-  AchievementCategory.create(name: "monsters", level: monster_level)
+  AchievementCategory.create(category: "monsters", level: monster_level)
 end
 
 ### CREATE USERS
 User.reindex
+Achievement.reindex
 puts "CREATING USERS"
 users_names = ["Guila", "Bruna", "Taynara","Miranda", "Joao", "Assis", "Dom", "Richard", "Kamilla", "Vitoria"]
 
@@ -26,6 +27,15 @@ users_names.each do |name|
   (1..50).each do |i|
     CollectedCoin.create(user: user, value: i * 1000)
   end
+  Achievement.create(user: user, category: 'monsters', level: 1)
+  Achievement.create(user: user, category: 'monsters', level: 100)
+  Achievement.create(user: user, category: 'monsters', level: 1000)
+  Achievement.create(user: user, category: 'coins', level: 1)
+  Achievement.create(user: user, category: 'coins', level: 100)
+  Achievement.create(user: user, category: 'coins', level: 1000)
+  Achievement.create(user: user, category: 'deaths', level: 1)
+  Achievement.create(user: user, category: 'deaths', level: 10)
+  Achievement.create(user: user, category: 'deaths', level: 25)
 end
 
 ## CREATE MONSTERS
@@ -47,3 +57,5 @@ KilledMonster.reindex
 monsters.each do |monster|
   KilledMonster.create(user: users[rand(1..users_count)], monster: monster)
 end
+
+
