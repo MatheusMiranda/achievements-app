34f344b8f969a289eb584ff74a0d3e16a02cea5f
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-09-11 17:46:17 -0300

    Update 'KilledMonster' creation route
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :killed_monsters, except: [:create]
-      post '/monster/:monster_id/user/:id', to: 'killed_monsters#create'
+      post '/monster/:monster_id/user/:user_id', to: 'killed_monsters#create'
       resources :monsters
 
       resources :users do
